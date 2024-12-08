0a3dba9972f4f9ce29b06f909bce476789bf4ff0
ansuehu <andonisudupeehu@gmail.com>
2024-12-08 21:21:57 +0100

    git config --global user.email "you@example.com"

--- proba_gehio.R
+++ proba_gehio.R
@@ -1,7 +1,7 @@
 a <- c(99, 100) == 100
 if (a) print('kaixo')
 
-x <- matrix(c(1,2,3,NA,5,6,7,8,9), nrow = 3)
+x <- matrix(c(1, 2, 3, NA, 5, 6, 7, 8, 9), nrow = 3)
 x
 measure <- x[upper.tri(x)]
 measure
@@ -11,12 +11,12 @@ measure
 library(cluster)
 library(sp)
 
-as.matrix <- function(x, ...){
-  d <- sqrt(length(x) * 2 +  length(attr(x, 'Labels')))
-  y <- matrix(attr(x, 'diagv'), d, d)
+as.matrix <- function(x, ...){ 
+  d <- sqrt(length(x) * 2 +  length(attr(x, "Labels")))
+  y <- matrix(attr(x, "diagv"), d, d)
   y[upper.tri(y)] <- x
   y[lower.tri(y)] <- t(y)[lower.tri(y)]
-  colnames(y) <- rownames(y) <- attr(x, 'Labels')
+  colnames(y) <- rownames(y) <- attr(x, "Labels")
   return (y)
 }
 
